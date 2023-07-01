caead7f475181d0bbc2c71d93eb91eb9eb588217
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-07-01 16:45:17 -0700

    data types

---
+++ README.txt
@@ -0,0 +1,2 @@
+to run a  specific port 
+flutter run -d chrome --web-port 8100
--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,147 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  variablePlayground();
+  untypedVariables();
+  typeInterpolation();
+  immutableVariables();
+  //strings
+  basicStringDeclaration();
+  multiLineStrings();
+  combiningStrings();
+  stringPlayground();
+
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void variablePlayground() {
+  basicTypes();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+void basicTypes() {
+  int four = 4;
+  double pi = 2.33;
+  num someNumber = 323432;
+  bool yes = true;
+  int? nothing;
+  print(nothing == null);
+  print(four);
+  print(pi);
+  print(yes);
+  print(someNumber);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+void untypedVariables() {
+  dynamic something = 14.2;
+  print(something.runtimeType); //outputs 'double'
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+void typeInterpolation() {
+  var anInteger = 15;
+  var aDouble = 27.6;
+  var aBoolean = false;
+  print(anInteger.runtimeType);
+  print(anInteger);
+  print(aDouble.runtimeType);
+  print(aDouble);
+  print(aBoolean.runtimeType);
+  print(aBoolean);
+}
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+void immutableVariables() {
+  final int immutableInteger = 5;
+  final double immutableDouble = 0.015;
+
+  // Type annotation is optional
+  final interpolatedInteger = 10;
+  final interpolatedDouble = 72.8;
+  print(interpolatedInteger);
+  print(interpolatedDouble);
+  const aFullySealedVariable = true;
+  print(aFullySealedVariable);
+
+  var animal = 'Duck';
+  final numValue = 42;
+  const isBoring = true;
+
+  print(animal);
+  print(numValue);
+  print(isBoring);
+}
+
+void basicStringDeclaration() {
+  // With Single Quotes
+  print('Single quotes');
+  final aBoldStatement = 'Dart isn\'t loosely typed.';
+  print(aBoldStatement);
+  //raw string
+  print('Raw String');
+  final rawString = r'Show an escape \ character';
+  print(rawString);
+  // With Double Quotes
+  print("Hello, World");
+  final aMoreMildOpinion = "Dart's popularity has skyrocketed with Flutter!";
+  print(aMoreMildOpinion);
+  // Combining single and double quotes 
+  final mixAndMatch =
+      'Every programmer should write "Hello, World" when learning a new language.';
+  print(mixAndMatch);
+}
+
+void multiLineStrings() {
+  final withEscaping = 'One Fish\nTwo Fish\nRed Fish\nBlue Fish';
+  print(withEscaping);
+// pay attention to any spaces left at the beginning 
+// of the lines when using ''' 
+  final hamlet = '''
+  To be, or not to be, that is the question:
+  Whether 'tis nobler in the mind to suffer
+  The slings and arrows of outrageous fortune,
+  Or to take arms against a sea of troubles
+  And by opposing end them.
+  ''';
+  print(hamlet);
+}
+
+void combiningStrings() {
+ traditionalConcatenation();
+ modernInterpolation();
+}
+void traditionalConcatenation() {
+ final hello = 'Hello';
+ final world = "world";
+ final combined = hello + ' ' + world;
+ print(combined);
+}
+void modernInterpolation() {
+ final year = 2011;
+ final interpolated = 'Dart was announced in $year.';
+ print(interpolated);
+ final age = 42;
+ final howOld = 'I am $age ${age == 1 ? 'year' : 'years'} old.';
+ print(howOld);
+}
+
+void   stringPlayground(){
+  // With Single Quotes
+final aBoldStatement = 'Dart isn\'t loosely typed.';
+// With Double Quotes 
+final opinion = "Dart's popularity has skyrocketed with Flutter!";
+
+final rawString = r'I want to show an escape \ character';
+
+final withEscaping = 'One Fish\nTwo Fish\nRed Fish\nBlue Fish';
+
+final hamlet = '''
+  To be, or not to be, that is the question:
+  Whether 'tis nobler in the mind to suffer
+  The slings and arrows of outrageous fortune,
+  Or to take arms against a sea of troubles
+  And by opposing end them.
+  ''';
+
+print (opinion);
+print(aBoldStatement);
+print (rawString);
+print(hamlet);
+print(withEscaping);
 }
